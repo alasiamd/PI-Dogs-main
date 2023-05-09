323c92ca7ed86be0314adbd4074fba10338f70c3
alasiamd <alasiamd@gmail.com>
2023-05-09 15:42:34 -0300

    Rutas back

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,10 +1,16 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const {
+    getDogs, getDogById, getDogByName, postDogs, getTemperaments
+} = require("../controllers/index");
 
 const router = Router();
-
+router.get("/dogs", getDogs);
+router.get("/dogs/:idRaza", getDogById);
+router.get("/dogs/", getDogByName);
+router.post("/dogs/", postDogs);
+router.get("/temperaments", getTemperaments);
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
