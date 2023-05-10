163b3bbcd79ed19e19af76a0e6835f679447d889
alasiamd <alasiamd@gmail.com>
2023-05-10 02:06:23 -0300

    Controller getDogById

--- api/src/controllers/getDogById.js
+++ api/src/controllers/getDogById.js
@@ -1,9 +1,33 @@
+const axios = require("axios");
+require('dotenv').config();
 
-const getDogById = async (req, res) => {
+const {API_KEY, ENDPOINT} = process.env;
+const getDogById = async (req, res) => {    
     try {
-        
+        const {idRaza} = req.params;
+        const {data} = await axios.get(`${ENDPOINT}/${idRaza}`);
+        console.log(data)
+        const {
+            id , 
+            reference_image_id, 
+            name, 
+            temperament, 
+            weight: {metric: weight},
+            height: {metric: height}, 
+            life_span
+        } = data               
+        res.status(200).json({
+            id, 
+            reference_image_id, 
+            name, 
+            weight, 
+            height,
+            temperament, 
+            life_span
+        })
     } catch (error) {
-                
+        console.log(error);
+        res.status(500).json({ message: error });                
     }
 };
 
