5394681e5a147f46192ae5f6075e5d7614260d25
alasiamd <alasiamd@gmail.com>
2023-05-10 00:05:36 -0300

    Controller getDogs

--- api/src/controllers/getDogs.js
+++ api/src/controllers/getDogs.js
@@ -1,10 +1,25 @@
+const axios = require("axios");
+require("dotenv").config();
 
+const { API_KEY, ENDPOINT } = process.env;
 const getDogs = async (req, res) => {
-    try {
-        
-    } catch (error) {
-                
-    }
+  try {
+    const { data } = await axios.get(`${ENDPOINT}`);
+    const dogs = data.map((element) => {
+      return {
+        id: element.id,
+        image: element.image.url,
+        name: element.name,
+        temperament: element.temperament,
+        weight: element.weight.metric,
+        height: element.height.metric,
+      };
+    });
+    res.status(200).json(dogs);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error });
+  }
 };
 
 module.exports = getDogs;
