c4ab8dae7bf3412fa9c4c1434597cd2a506049e0
alasiamd <alasiamd@gmail.com>
2023-05-12 15:19:49 -0300

    Controller getDog(con query)

--- api/src/controllers/getDogs.js
+++ api/src/controllers/getDogs.js
@@ -1,24 +1,48 @@
 const axios = require("axios");
 require("dotenv").config();
+const { Dog } = require("../db");
 
 const { API_KEY, ENDPOINT } = process.env;
 const getDogs = async (req, res) => {
-  try {
-    const { data } = await axios.get(`${ENDPOINT}`);
-    const dogs = data.map((element) => {
-      return {
-        id: element.id,
-        image: element.image.url,
-        name: element.name,
-        temperament: element.temperament,
-        weight: element.weight.metric,
-        height: element.height.metric,
-      };
-    });
-    res.status(200).json(dogs);
+  const { search } = req.query;
+  console.log(search);
+      try {
+          if (search) {
+          const { data } = await axios.get(`${ENDPOINT}/search?q=${search}&api_key=${API_KEY}`)
+              const infoApi = data.map(element => {
+                  return {
+                      id: element.id,
+                      image: element.reference_image_id,
+                      name: element.name,
+                      temperament: element.temperament,
+                      weight: element.weight.metric,
+                      height: element.height.metric,
+                      life_span: element.life_span,
+                  }
+              });
+
+          res.status(200).json(infoApi)
+
+      } else {
+          const { data } = await axios.get(`${ENDPOINT}`)
+          const infoApi = data.map(element => {
+              return {
+                  id: element.id,
+                  image: element.image.url,
+                  name: element.name,
+                  temperament: element.temperament,
+                  weight: element.weight.metric,
+                  height: element.height.metric,
+                  life_span: element.life_span,
+              }
+          });
+
+          res.status(200).json(infoApi)
+
+      }
+
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: error });
+      res.status(500).json({ error: error.message });
   }
 };
 
