4b9b85df4617b96cea6df295fa97968b420db72f
alasiamd <alasiamd@gmail.com>
2023-05-16 17:38:57 -0300

    getDogs(con los registros de la Api y la tabla)

--- api/src/controllers/getDogs.js
+++ api/src/controllers/getDogs.js
@@ -1,48 +1,25 @@
 const axios = require("axios");
 require("dotenv").config();
-const { Dog } = require("../db");
+const { Dog, Temperament } = require("../db");
+const { handlerGetDogs } = require("../handlers/index");
 
 const { API_KEY, ENDPOINT } = process.env;
 const getDogs = async (req, res) => {
   const { search } = req.query;
-  console.log(search);
-      try {
-          if (search) {
-          const { data } = await axios.get(`${ENDPOINT}/search?q=${search}&api_key=${API_KEY}`)
-              const infoApi = data.map(element => {
-                  return {
-                      id: element.id,
-                      image: element.reference_image_id,
-                      name: element.name,
-                      temperament: element.temperament,
-                      weight: element.weight.metric,
-                      height: element.height.metric,
-                      life_span: element.life_span,
-                  }
-              });
-
-          res.status(200).json(infoApi)
-
-      } else {
-          const { data } = await axios.get(`${ENDPOINT}`)
-          const infoApi = data.map(element => {
-              return {
-                  id: element.id,
-                  image: element.image.url,
-                  name: element.name,
-                  temperament: element.temperament,
-                  weight: element.weight.metric,
-                  height: element.height.metric,
-                  life_span: element.life_span,
-              }
-          });
-
-          res.status(200).json(infoApi)
-
-      }
-
+  const dogs = await handlerGetDogs();
+  try {
+    if (search) {
+      let searchedDog = dogs.filter((element) =>
+        element.name.toLowerCase().includes(search.toLowerCase())
+      );
+      searchedDog.length
+        ? res.status(200).json(searchedDog)
+        : res.status(404).json({ message: "Personaje no encontrado" });
+    } else {
+      res.status(200).json(dogs);
+    }
   } catch (error) {
-      res.status(500).json({ error: error.message });
+    res.status(500).json({ error: error.message });
   }
 };
 
--- api/src/handlers/handlerGetDogs.js
+++ api/src/handlers/handlerGetDogs.js
@@ -1,4 +1,43 @@
-const { Dog } = require('../db');
-const handlerGetDog = async (req, res) => {
-    const data = await Dog.findAll()
+const axios = require("axios");
+require("dotenv").config();
+const { Dog, Temperament } = require("../db");
+const { API_KEY, ENDPOINT } = process.env;
+
+const getApi = async () => {
+    const { data } = await axios.get(`${ENDPOINT}`)
+    const infoApi = data.map(element => {
+        return {
+            id: element.id,
+            image: element.image.url,
+            name: element.name,
+            temperament: element.temperament,
+            weight: element.weight.metric,
+            height: element.height.metric,
+            life_span: element.life_span,
+            temperament: element.temperament
+        }        
+    });  
+    return infoApi;      
 }
+
+const getDB = async () => {
+    return await Dog.findAll({
+        include: {
+            model: Temperament,
+            attributes: ['name'],
+            through: {
+                attributes: [],
+            },
+        }        
+    })    
+}
+
+const handlerGetDogs = async () => {
+    const apiInfo = await getApi();
+    const dbInfo = await getDB();
+    console.log(dbInfo);
+    const total = apiInfo.concat(dbInfo)
+    return total;
+}
+
+module.exports = handlerGetDogs;
