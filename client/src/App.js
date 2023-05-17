400e2ba19e0d946c8d08041a816b20139065359c
alasiamd <alasiamd@gmail.com>
2023-05-17 04:52:37 -0300

    Ruteo basico del front

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
+import { Routes, Route, useLocation } from 'react-router-dom';
 import './App.css';
+import LandingPage from './components/landing-page/landingPage';
+import HomePage from './components/home_page/homePage';
+import Details from './components/details/details';
+import NewDog from './components/new_dog/newDog';
+import NavBar from './components/nav_bar/navBar';
 
 function App() {
+  const location = useLocation();
+
   return (
-    <div className="App">
-      <h1>Henry Dogs</h1>
+    <div className='App'>
+    {location.pathname !== "/landing" && <NavBar />}
+    <Routes>
+      <Route path="/landing" element={<LandingPage />} />
+      <Route path="/home" element={<HomePage />} />
+      <Route path="/details/:id" element={<Details />} />
+      <Route path="/new" element={<NewDog />} />
+    </Routes>
     </div>
+
+
   );
 }
 
