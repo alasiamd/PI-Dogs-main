3811b66913de8ed5410db93693286c815c8a9261
alasiamd <alasiamd@gmail.com>
2023-05-18 21:13:05 -0300

    Mejorando el renderizado de la Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import LandingPage from './components/landing-page/landingPage';
 import HomePage from './components/home_page/homePage';
@@ -7,16 +7,20 @@ import NewDog from './components/new_dog/newDog';
 import NavBar from './components/nav_bar/navBar';
 
 function App() {
-  const location = useLocation();
+  // const location = useLocation();
+  
 
   return (
     <div className='App'>
-    {location.pathname !== "/landing" && <NavBar />}
+    {/* {location.pathname !== "/" && <NavBar />} */}
+    
+    <NavBar />
     <Routes>
-      <Route path="/landing" element={<LandingPage />} />
+      <Route path="/" element={<LandingPage />} />
       <Route path="/home" element={<HomePage />} />
       <Route path="/details/:id" element={<Details />} />
       <Route path="/new" element={<NewDog />} />
+      <Route path="*" element={<LandingPage />} />
     </Routes>
     </div>
 
--- client/src/components/nav_bar/navBar.jsx
+++ client/src/components/nav_bar/navBar.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import styles from './navBar.module.css';
+import { useLocation } from 'react-router-dom';
 
 const NavBar = () => {   
+  const location = useLocation();
 
-
-
-  return (
-    <div className={styles.card}>
-      {/* Contenido del componente */}
-    </div>
-  );
+  if (location.pathname === '/home' || location.pathname === '/new') {
+    return (
+    
+      <div className={styles.card}>
+        Nav component
+      </div>
+    );
+  }
+  else {
+    return (
+    <></>
+    );
+  }
+  
 };
 
 export default NavBar;
