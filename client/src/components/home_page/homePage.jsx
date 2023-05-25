48d1be1a3fdfcd2c2a86ec1f46c1763dfc3ce20c
alasiamd <alasiamd@gmail.com>
2023-05-25 04:49:21 -0300

    Paginado

--- client/src/components/home_page/homePage.jsx
+++ client/src/components/home_page/homePage.jsx
@@ -1,32 +1,68 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './homePage.module.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { all } from '../../reducer/action';
 import Card from '../card/card'
+import { ALL } from '../../reducer/types';
 
 const HomePage = () => {   
   const dispatch = useDispatch();
+  const [currentPage, setCurrentPage] = useState(1);
+  const [currentCards, setCurrenCards] = useState([]);
+
+  const allReg = useSelector(state => state.all);
+  const cardsPerPage = 8;
+
   useEffect(() => {
     dispatch(all())
   },[])
 
-  const allReg = useSelector(state => state.all);
-  console.log(allReg);
-  return (
-    <div className={styles.card}>
-      {allReg.map(({ id, image, life_span, name, temperament, weight, height }) => <Card 
-        id = {id}
-        key = {id}
-        image = {image}
-        life_span = {life_span}
-        name = {name}
-        temperament = {temperament}
-        weight = {weight}
-        height = {height}
-      />)}
-      
+  useEffect(() => {
+    const indexOfLastCard = currentPage * cardsPerPage;
+    const indexOfFirstCard = indexOfLastCard - cardsPerPage;
+    const currentCards = allReg.slice(indexOfFirstCard, indexOfLastCard);
+    setCurrenCards(currentCards);
+  }, [currentPage, allReg])
 
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  const totalPages = Math.ceil(allReg.length / cardsPerPage);
+  const pageNumbers = [];
+
+  for (let i = currentPage - 2; i <= currentPage + 2; i++) {
+    if (i >= 1 && i <= totalPages) {
+      pageNumbers.push(i);      
+    }
+  }
+
+  return (
+    <>
+      <div className={styles.content}>
+        {currentCards.map(({ id, image, life_span, name, temperament, weight, height }) => (
+          <Card
+            id={id}
+            key={id}
+            image={image}
+            life_span={life_span}
+            name={name}
+            temperament={temperament}
+            weight={weight}
+            height={height}
+          />
+        ))}
     </div>
+      <div className={styles.pagination}>
+        {pageNumbers.map((pageNumber) => (
+          <button 
+            key={pageNumber} 
+            className={currentPage === pageNumber ? styles.activePage : ''}
+              onClick={() => paginate(pageNumber)}
+          >
+            {pageNumber}
+          </button>
+        ))}
+      </div>
+    </>
   );
 };
 
