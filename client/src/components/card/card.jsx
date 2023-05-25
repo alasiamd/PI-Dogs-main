1d7269b672f0847b4548d909defc4557ecb4654e
alasiamd <alasiamd@gmail.com>
2023-05-25 03:30:49 -0300

    Estructura basica de las cards

--- client/src/components/card/card.jsx
+++ client/src/components/card/card.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import styles from './card.module.css';
 
-const Card = (props) => {   
-
-
-
+const Card = ({ id, image, life_span, name, temperament, weight, height }) => {
   return (
     <div className={styles.card}>
-      <h1>{props.name} </h1>
+      <div className={styles.imageContainer}>
+        <img src={image} alt={name} className={styles.image} />
+      </div>
+      <h2>{name}</h2>
+      <p>Life Span: {life_span}</p>
+      <p>Temperament: {temperament}</p>
+      <p>Weight: {weight}</p>
+      <p>Height: {height}</p>
+      <button>View Details</button>
     </div>
   );
 };
--- client/src/components/landing-page/landingPage.jsx
+++ client/src/components/landing-page/landingPage.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './landingPage.module.css';
+import logo from '../../assets/logo.jpeg'
+import lucy from '../../assets/lucy.jpeg'
 
 const LandingPage = () => {   
 
 
 
   return (
-    <div className={styles.card}>
-      Landing page component
+    <div className={styles.container}>
+      <div className={styles.background}></div>
+      <div className={styles.logoContainer}>
+        <img src={logo} alt="Logo" className={styles.logo} />
+      </div>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Welcome to Our Website</h1>
+        <p className={styles.subtitle}>Discover the Fascinating World of Dogs</p>
+        <img src={lucy} alt="Representative Photo" className={styles.representativePhoto} />
+        <Link to="/home" className={styles.button}>Go to Home</Link>
+      </div>
     </div>
   );
 };
