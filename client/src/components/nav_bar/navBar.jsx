00962b282d13d881898271ef952e92d4d6c8dc3f
alasiamd <alasiamd@gmail.com>
2023-05-25 07:02:34 -0300

    Maquetado de Navbar

--- client/src/components/nav_bar/navBar.jsx
+++ client/src/components/nav_bar/navBar.jsx
@@ -1,24 +1,40 @@
 import React from 'react';
+import { useLocation, Link } from 'react-router-dom';
 import styles from './navBar.module.css';
-import { useLocation } from 'react-router-dom';
+import logo from '../../assets/logo.jpeg';
 
-const NavBar = () => {   
+const NavBar = () => {
   const location = useLocation();
 
   if (location.pathname === '/home' || location.pathname === '/new') {
     return (
-    
-      <div className={styles.card}>
-        Nav component
-      </div>
+      <header className={styles.header}>
+        <div className={styles.logo}>
+          <img src={logo} alt="Logo" className={styles.logoImage} />
+        </div>
+        <nav className={styles.navbar}>
+          <input type="text" placeholder="Buscar" className={styles.searchInput} />
+          <select className={styles.select}>
+            <option value="">Filtrar por temperamento</option>
+            {/* Opciones del select */}
+          </select>
+          <select className={styles.select}>
+            <option value="asc">Ordenar A-Z</option>
+            <option value="desc">Ordenar Z-A</option>
+          </select>
+          <select className={styles.select}>
+            <option value="all">Todos</option>
+            <option value="new">Nuevos</option>
+          </select>
+          <Link to="/new" className={styles.link}>
+            Agregar nueva raza
+          </Link>
+        </nav>
+      </header>
     );
+  } else {
+    return <></>;
   }
-  else {
-    return (
-    <></>
-    );
-  }
-  
 };
 
 export default NavBar;
