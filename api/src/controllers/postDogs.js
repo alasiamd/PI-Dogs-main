d08743caca0d6a927728a353dbcb1694b55e0312
alasiamd <alasiamd@gmail.com>
2023-05-26 05:00:30 -0300

    Agregamos la funcion next()

--- api/src/controllers/postDogs.js
+++ api/src/controllers/postDogs.js
@@ -1,14 +1,7 @@
 const { handlerPostDogs } = require("../handlers/index");
 
-const postDogs = async (req, res) => {
-  const { 
-          name, 
-          image, 
-          height, 
-          weight, 
-          life_span, 
-          temperament 
-        } = req.body;
+const postDogs = async (req, res, next) => {
+  const { name, image, height, weight, life_span, temperament } = req.body;
   try {
     if (!name || !image || !height || !weight || !life_span || !temperament)
       res.status(401).json({ message: "faltan datos" });
@@ -22,9 +15,10 @@ const postDogs = async (req, res) => {
       temperament
     );
 
-    res.status(200).json(data);
+    req.responseData = data; // Guardar los datos en la solicitud para usarlos en el siguiente middleware
+    next();
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    next(error); // Pasar el error al siguiente middleware de manejo de errores
   }
 };
 
