730fffba1d72fa66bc1462f62bdc4ce618a3f902
alasiamd <alasiamd@gmail.com>
2023-05-28 03:02:44 -0300

    Se agrega el componente Details reutilizando Card

--- client/src/components/card/card.jsx
+++ client/src/components/card/card.jsx
@@ -1,18 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import styles from './card.module.css';
 
 const Card = ({ id, image, life_span, name, temperament, weight, height }) => {
+  const [errorImg, setErrorImg] = useState(false);
+  const [ errorImg2, setErrorImg2 ] = useState(false);
+  const imgURL = "https://cdn2.thedogapi.com/images/";
+
+  const handleImgError = () => {
+    setErrorImg(true);
+  }
+
+  const handleImgError2 = () => {
+    setErrorImg2(true);
+  }
+
+  const imagen = () => {
+    if (!errorImg2) {
+      return <img src={image} alt={name} className={styles.image} onError={handleImgError2}/>
+    }
+    else if (!errorImg) {
+      return <img src={`${imgURL}${image}.jpg`} alt={name} className={styles.image} onError={handleImgError}/>
+    } else {
+      return <img src={`${imgURL}${image}.png`} alt={name} className={styles.image} />
+    }
+  }
+
   return (
+    // `${imgURL}${dogId.reference_image_id}.jpg`
     <div className={styles.card}>
       <div className={styles.imageContainer}>
-        <img src={image} alt={name} className={styles.image} />
+        {imagen() }
       </div>
       <h2>{name}</h2>
       <p>Life Span: {life_span}</p>
       <p>Temperament: {temperament}</p>
       <p>Weight: {weight}</p>
       <p>Height: {height}</p>
-      <button>View Details</button>
+      <Link to={`/details/${id}`} className={styles.button} >
+        View Details
+      </Link>
+      {/* <button>View Details</button> */}
     </div>
   );
 };
--- client/src/components/details/details.jsx
+++ client/src/components/details/details.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { searchId } from '../../reducer/action';
 import styles from './details.module.css';
+import Card from '../card/card';
 
 const Details = () => {   
   const { id } = useParams();
@@ -12,14 +13,23 @@ const Details = () => {
     dispatch(searchId(id))
   }, [dispatch, id]);
 
+ 
+
+
 const dogId = useSelector(state => state.searchId);
-console.log(dogId);
-
-
-
+  console.log(dogId)
   return (
     <div className={styles.card}>
-      Details component
+      <Card
+            id={dogId.id}
+            key={dogId.id}
+            image={dogId.reference_image_id}
+            life_span={dogId.life_span}
+            name={dogId.name}
+            temperament={dogId.temperament}
+            weight={dogId.weight}
+            height={dogId.height}
+          />
     </div>
   );
 };
