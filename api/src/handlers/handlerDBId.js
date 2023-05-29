e71318575b1ca78c0c26cf43331ab2e4ef7c183f
alasiamd <alasiamd@gmail.com>
2023-05-29 03:24:55 -0300

    Mejora la manera de traer temperamentos en registros nuevos

--- api/src/handlers/handlerDBId.js
+++ api/src/handlers/handlerDBId.js
@@ -1,17 +1,30 @@
 const { Dog, Temperament } = require("../db");
 
 const handlerDBId = async (id) => {
-    const data = await Dog.findAll({
-        where: { id },
-        include: {
-            model: Temperament,
-            attributes: ['name'],
-            through: {
-                attributes: [],
-            },
-        }   
-    })    
-    return data;     
-}
+  const data = await Dog.findAll({
+    where: { id },
+    include: {
+      model: Temperament,
+      attributes: ["name"],
+      through: {
+        attributes: [],
+      },
+    },
+  });
+  const value = data.map((element) => {
+    const temperaments = element.temperaments.map((t) => t.name);
+    return {
+      id: element.id,
+      image: element.image,
+      name: element.name,
+      weight: element.weight.metric,
+      height: element.height.metric,
+      life_span: element.life_span,
+      temperament: temperaments.join(", "),
+      createdInDb: true,
+    };
+  });
+  return value;
+};
 
 module.exports = handlerDBId;
